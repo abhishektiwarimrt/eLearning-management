c51f3f6343c074d227a73bb2d0a868033fa98a98
Abhishek <abhishektiwarimrt@users.noreply.github.com>
2024-07-02 23:13:06 +0530

    Basic Fluent Validation

--- src/Services/UserManagement/lms.usermanagement.api/Profiles/CreateProfile/CreateUserProfileHandler.cs
+++ src/Services/UserManagement/lms.usermanagement.api/Profiles/CreateProfile/CreateUserProfileHandler.cs
@@ -1,15 +1,35 @@
 ﻿
 
+using FluentValidation;
+
 namespace lms.usermanagement.api.Profiles.CreateProfile
 {
     public record CreateUserProfileCommand(string FirstName, string LastName, string Title, string Address, string City, string Country)
     : IRequest<CreateUserProfileResult>;
     public record CreateUserProfileResult(Guid Id);
-    internal class CreateUserProfileCommandHandler(IDocumentSession session)
+
+    public class CreateUserProfileCommandValidator : AbstractValidator<CreateUserProfileCommand>
+    {
+        public CreateUserProfileCommandValidator()
+        {
+            RuleFor(x => x.FirstName).NotEmpty().WithMessage("First Name is required");
+            RuleFor(x => x.LastName).NotEmpty().WithMessage("Last Name is required");
+        }
+    }
+
+    internal class CreateUserProfileCommandHandler(IDocumentSession session, IValidator<CreateUserProfileCommand> validator)
         : IRequestHandler<CreateUserProfileCommand, CreateUserProfileResult>
     {
         public async Task<CreateUserProfileResult> Handle(CreateUserProfileCommand command, CancellationToken cancellationToken)
         {
+
+            var result = await validator.ValidateAsync(command, cancellationToken);
+            var errors = result.Errors.Select(x => x.ErrorMessage).ToList();
+            if (errors.Any())
+            {
+                throw new System.ComponentModel.DataAnnotations.ValidationException(errors.FirstOrDefault());
+            }
+
             //Create Entity from command object
             //Save Entity
             //Retrun result
